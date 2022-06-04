c4824af53c85d1dca692a8bd8ba900733aa3fbf2
Adriano Pontes <ampontes93@gmail.com>
2022-06-04 11:37:42 -0300

    ajustando gateway

--- supermarket-acme/gateway/src/main/java/br/com/gateway/GatewayApplication.java
+++ supermarket-acme/gateway/src/main/java/br/com/gateway/GatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ApiGatewayApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
